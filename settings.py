9ea73f7b41cb9973bb83237447f2ac90bcd50bb6
rimba28 <rimba28oktober@gmail.com>
2018-04-12 16:28:35 +0700

    rimba branch original

--- settings.py
+++ settings.py
@@ -159,6 +159,7 @@ LOGIN_URL = reverse_lazy('login') #this for custom login url e.g: localhost:8000
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
 #This is Rimba's branch
+#Ok This is original
 
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
