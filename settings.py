da2dac240a0ecc6c51b1a5adccef374a0f090df3
rimba28 <rimba28oktober@gmail.com>
2018-04-12 15:18:31 +0700

    my testss

--- settings.py
+++ settings.py
@@ -158,9 +158,6 @@ LOGIN_URL = reverse_lazy('login') #this for custom login url e.g: localhost:8000
 
 #STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CachedStaticFilesStorage'
 
-
-#This is learn 1
-
 EMAIL_USE_TLS=True
 EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
